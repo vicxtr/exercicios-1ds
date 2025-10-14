edb8dad27f72c5e22787f890c62e2f353630f05b
João Victor <joao.silva4011@aluno.ce.gov.br>
2025-10-14 14:41:03 +0000

    atualizado !

--- 4-bimestre/ex03-loop-for/index.js
+++ 4-bimestre/ex03-loop-for/index.js
@@ -1,4 +1,11 @@
 // CRIE UMA LÓGICA AQUI
+let resultado = 0
+
+for(let i = 1 ; i <= 10 ; i ++){
+    resultado += i
+}
+
+console.log(`resultado:${resultado}`)
 
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = resultado
